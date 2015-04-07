4863b5d2336d5e8adf67c8178c7fc115f8beca0a
AdityaMishra <aditya.mishra@ischool.berkeley.edu>
2015-04-06 21:55:05 -0700

    second push

--- bart/apiLayer.py
+++ bart/apiLayer.py
@@ -120,17 +120,23 @@ def getinComingTrainIDsForStation(abbr):
 def getLoadFactor(abbr,trainID):
 	apikey='ZMVV-JDE7-IQ9Q-DT35'
 	routePad=trainID.split('-')[0]
+	routePad2=trainID.split('-')[1]
 	baseURL='http://api.bart.gov/api/sched.aspx?cmd=load'
 	if(len(routePad)==1):
 		routePad='0'+routePad
+	if(len(routePad2)==1):
+		routePad2='0'+routePad2
 	print "load factor with -", abbr+routePad+trainID.split('-')[1]
-	data1={'key':apikey, 'ld1':abbr+routePad+trainID.split('-')[1],'st':'w'}
+	data1={'key':apikey, 'ld1':abbr+routePad+routePad2,'st':'w'}
 	r=requests.get(baseURL,params=data1)
 	#r=requests.post(baseURL,data=data1)
 	#r=requests.post(baseURL,data=data1)
 	#print r
-	tree = ElementTree.fromstring(r.content)
-	print "respinse",r.content
-	node=dict(tree.find('load').find('request').find('leg').attrib)
-	retDict={'1':'Light','2':'Medium','3':'Heavy'}
-	return retDict[node['load']]
+	try:
+		tree = ElementTree.fromstring(r.content)
+		print "respinse",r.content
+		node=dict(tree.find('load').find('request').find('leg').attrib)
+		retDict={'1':'Light','2':'Medium','3':'Heavy'}
+		return retDict[node['load']]
+	except ValueError:
+		return "Unknown"
